2ee8fef86afc3d170b4f4cce1949d0632f98ba16
Mitchell57 <mitchell5799@gmail.com>
2016-09-02 10:50:19 -0700

    Added Files

---
+++ APLesson_01/asciiArtOne.java
@@ -0,0 +1,11 @@
+public class asciiArtOne
+{
+	public static void main(String[] args){
+		System.out.println("    ____________________");
+		System.out.println("   /  __________________()");
+		System.out.println("  /  / |_________________ ");
+		System.out.println(" /  /_/________________()");
+		System.out.println("/______________________");
+		System.out.println("|_____________________()");
+	}
+}
---
+++ APLesson_01/asciiArtTwo.java
@@ -0,0 +1,13 @@
+public class asciiArtTwo
+{
+	public static void main(String[] args){
+		System.out.println("._____. ._____.");
+		System.out.println("| ._. | | ._. |");
+		System.out.println("| !_| |_|_|_! |");
+		System.out.println("!___| |_______!");
+		System.out.println("!___| |_______!");
+		System.out.println("| ._____| |_. |");
+		System.out.println("| !_! | | !_! |");
+		System.out.println("!_____! !_____!");
+	}
+}
